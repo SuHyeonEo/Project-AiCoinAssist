240b13cacba5007f93ff8c209667a8a796c36e73
SuHyeonEo <tngus7520@gmail.com>
2026-03-07 19:07:19 +0900

    chore: init

---
+++ src/main/java/com/aicoinassist/batch/BatchApplication.java
@@ -0,0 +1,13 @@
+package com.aicoinassist.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ggBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchApplication.class, args);
+	}
+
+}
