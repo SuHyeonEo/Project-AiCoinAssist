675231a7e53e59ad8ad01041c04d8fa78dd6e94d
SuHyeonEo <tngus7520@gmail.com>
2026-03-09 03:45:55 +0900

    test: replace mockbean in admin api test

--- src/test/java/com/aicoinassist/batch/domain/report/controller/AnalysisReportBatchAdminControllerTest.java
+++ src/test/java/com/aicoinassist/batch/domain/report/controller/AnalysisReportBatchAdminControllerTest.java
@@ -19,9 +19,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.context.TestPropertySource;
 
@@ -54,10 +54,10 @@ class AnalysisReportBatchAdminControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private AnalysisReportBatchRunReadService analysisReportBatchRunReadService;
 
-    @MockBean
+    @MockitoBean
     private AnalysisReportBatchRerunService analysisReportBatchRerunService;
 
     @Test
